55c39442d9d3e1cbf6129dd3b33a89b8da36e1b3
alextrujillo <alextrujillo72002@gmail.com>
2024-06-18 19:33:18 +0200

    feat: loginView

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return Response({"message": "Successful login"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.user',
+    'rest_framework',
+    'apps.user'
 ]
 
 MIDDLEWARE = [
