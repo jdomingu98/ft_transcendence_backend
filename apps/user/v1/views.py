29a8e2a479f68a34ce2d377e883c98ce764c97f5
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-06-19 21:42:50 +0200

    feat: add simple endpoint for register

---
+++ apps/user/v1/views.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from ..enums import Visibility
+
+from apps.user.models import User
+
+class Register(APIView):
+
+    def post(self, request):
+        # TODO: Review params for endpoint
+        name = request.data.get('name')
+        password = request.data.get('password')
+        passwordRepeat = request.data.get('passwordRepeat')
+
+        if password != passwordRepeat:
+            return Response({'message': 'passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # TODO: Store password hashed
+        user = User(
+            username=name,
+            password=password,
+            language='es',
+            visibility=Visibility.PUBLIC.value
+        )
+        user.save()
+        return Response({'message': 'you are register'}, status=status.HTTP_200_OK)
+
+
+
