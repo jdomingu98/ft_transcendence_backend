b8630321260942f1d5c183bb7f296f98abcf4501
cmorales <moralesrojascr@gmail.com>
2024-07-03 19:33:04 +0200

    fix: all pull request issues solved
    Implement 0 to 100 limiter, default value of time played was removed, and lastly extra spaces also was eliminated.

--- apps/game/models.py
+++ apps/game/models.py
@@ -1,28 +1,27 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Statistics(models.Model):
-    user_id = models.OneToOneField('user.User', on_delete=models.CASCADE)
+    user = models.OneToOneField('user.User', on_delete=models.CASCADE)
 
-    max_streak = models.IntegerField()
+    max_streak = models.IntegerField(default=0)
 
-    invididual_win_rate = models.IntegerField()
+    invididual_win_rate = models.IntegerField(default=100, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
-    tournament_win_rate = models.IntegerField()
+    tournament_win_rate = models.IntegerField(default=100, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     time_played = models.TimeField()
     
-    punctuation = models.IntegerField()
+    punctuation = models.IntegerField(default=0)
 
-    total_match = models.IntegerField()
+    total_match = models.IntegerField(default=0)
 
-    total_turney = models.IntegerField()
+    total_turney = models.IntegerField(default=0)
 
-    total_goals = models.IntegerField()
+    total_goals = models.IntegerField(default=0)
 
-    total_goals_against = models.IntegerField()
+    total_goals_against = models.IntegerField(default=0)
 
-    total_goals_stopped = models.IntegerField()
-
+    total_goals_stopped = models.IntegerField(default=0)
