30d79e715b354e93f90e049a17c0ed1b824e275e
alextrujillo <alextrujillo72002@gmail.com>
2024-07-05 09:40:02 +0200

    feat: logoutView def post

--- apps/user/v1/urls.py
+++ apps/user/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from .views import Register, LoginView
+from .views import Register, LoginView, LogoutView
 
 urlpatterns = [
     re_path(r'^register/$', Register.as_view()),
     re_path(r'^login/$', LoginView.as_view()),
+    re_path(r'^logout/$', LogoutView.as_view()),
 ]
--- apps/user/v1/views.py
+++ apps/user/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import logout
 from .serializers import RegisterSerializer, LoginSerializer
 from ..models import User
 
@@ -18,3 +19,13 @@ class LoginView(APIView):
             return Response({"message": "Successful login"}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    def post(self, request):
+        if request.user.is_authenticated:
+            request.user.is_connected = False
+            request.user.save()
+            logout(request)
+            return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Not logged in."}, status=status.HTTP_400_BAD_REQUEST)
