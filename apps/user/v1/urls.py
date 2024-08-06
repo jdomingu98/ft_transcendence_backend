c06b01a1012b67b4abab7fce9bba74d980fed86e
alextrujillo <alextrujillo72002@gmail.com>
2024-08-06 20:46:41 +0200

    feat: add URL route for RefreshTokenView

--- apps/user/v1/urls.py
+++ apps/user/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Register, LoginView, PasswordResetView
+from .views import Register, LoginView, PasswordResetView, RefreshTokenView, MeView
 
 urlpatterns = [
     path('register/', Register.as_view()),
     path('login/', LoginView.as_view()),
     path('pass-reset', PasswordResetView.as_view()),
+    path('refresh/', RefreshTokenView.as_view()),
+    path('me/', MeView.as_view()),
 ]
