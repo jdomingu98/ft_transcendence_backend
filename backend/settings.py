fa487e8d0235ef5fd0a6c034a060914fb50b4e7d
Juan Antonio Domínguez <jadominguezarcas@gmail.com>
2024-09-28 19:54:18 +0200

    fix(env variables): change variables to match postgres image

--- backend/settings.py
+++ backend/settings.py
@@ -110,9 +110,9 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": env("DB_ENGINE"),
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWD"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
         "HOST": env("DB_HOST"),
         "PORT": env("DB_PORT"),
     }
