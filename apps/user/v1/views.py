f223a4e8af893486d15a5b846c146410d92bb6be
cmorales <moralesrojascr@gmail.com>
2024-10-03 18:29:11 +0200

    feat(OTP): create OTP endpoint

--- apps/user/v1/views.py
+++ apps/user/v1/views.py
@@ -8,8 +8,9 @@ from django.db.models import Q
 from backend.utils.jwt_tokens import verify_token
 from backend.utils.oauth_utils import get_access_token, get_user_info, get_or_create_user
 from backend.utils.pass_reset_utils import send_reset_email
+from backend.utils.otp_utils import send_otp_code, verify_otp_code
 
-from ..models import RefreshToken, User
+from ..models import RefreshToken, User, OTPCode
 from .serializers import (
     LoginSerializer,
     LogoutSerializer,
@@ -23,14 +24,16 @@ from .serializers import (
     UserListSerializer,
     UserSerializer,
     MeNeedTokenSerializer,
+    OTPSerializer,
 )
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all().order_by("username")
 
+    serializer_class = UserSerializer
     def get_serializer_class(self):
-        serializer = UserSerializer
+        serializer = super().get_serializer_class()
         if self.action == "create":
             serializer = RegisterSerializer
         elif self.action == "retrieve":
@@ -47,8 +50,26 @@ class UserViewSet(ModelViewSet):
     def login(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
+            user: User = serializer.user
+            if user.two_factor_enabled:
+                send_otp_code(user)
+                return Response(status=status.HTTP_204_NO_CONTENT)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(methods=["POST"], detail=False, url_path="otp", url_name="otp",serializer_class=OTPSerializer)
+    def otp(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data["username"]
+            user = User.objects.get(username=username)
+            try:
+                if(verify_otp_code(user, request.data["code"])):
+                    return Response(LoginSerializer(user).data, status=status.HTTP_200_OK)
+            except Exception as e:
+                return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     @action(methods=["POST"], detail=False, url_path="me", url_name="me", serializer_class=MeNeedTokenSerializer)
     def me(self, request):
@@ -57,29 +78,19 @@ class UserViewSet(ModelViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(verify_token(request.data["token"]), status=status.HTTP_200_OK)
 
-    @action(
-        methods=["POST"],
-        detail=False,
-        url_path="pass-reset",
-        url_name="pass-reset",
-        serializer_class=PasswordResetSerializer,
-    )
+    @action(methods=["POST"], detail=False,url_path="pass-reset",url_name="pass-reset", serializer_class=PasswordResetSerializer)
     def password_reset(self, request):
         try:
             serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-
+            serializer.is_valid()
             username_or_email = serializer.validated_data["username_or_email"]
             user = get_object_or_404(User, Q(email=username_or_email) | Q(username=username_or_email))
             send_reset_email(user)
-
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Exception:
-            return Response({"error": "ERROR.PASSWORD_RESET"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(
-        methods=["POST"], detail=False, url_path="refresh", url_name="refresh", serializer_class=RefreshTokenSerializer
-    )
+    @action(methods=["POST"], detail=False, url_path="refresh", url_name="refresh", serializer_class=RefreshTokenSerializer)
     def refresh(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
