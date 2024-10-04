6b1dd765060713801478cfeb3b4351a4fdeb005e
cmorales <moralesrojascr@gmail.com>
2024-10-04 15:33:18 +0200

    refactor(views): simplify handling 400 error

--- apps/user/v1/views.py
+++ apps/user/v1/views.py
@@ -49,44 +49,37 @@ class UserViewSet(ModelViewSet):
     @action(methods=["POST"], detail=False, url_path="login", url_name="login", serializer_class=LoginSerializer)
     def login(self, request):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user: User = serializer.user
-            if user.two_factor_enabled:
-                send_otp_code(user)
-                return Response(status=status.HTTP_204_NO_CONTENT)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user: User = serializer.user
+        if user.two_factor_enabled:
+            send_otp_code(user)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     @action(methods=["POST"], detail=False, url_path="otp", url_name="otp",serializer_class=OTPSerializer)
     def otp(self, request):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            username = serializer.validated_data["username"]
-            user = User.objects.get(username=username)
-            if(verify_otp_code(user, request.data["code"])):
-                return Response(LoginSerializer(user).data, status=status.HTTP_200_OK)
-            return Response({"error": "ERROR.OTP.CODE"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data["username"]
+        user = get_object_or_404(User,username=username)
+        if(verify_otp_code(user, request.data["code"])):
+            return Response(LoginSerializer(user).data, status=status.HTTP_200_OK)
+        return Response({"error": "ERROR.OTP.CODE"}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(methods=["POST"], detail=False, url_path="me", url_name="me", serializer_class=MeNeedTokenSerializer)
     def me(self, request):
         serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
         return Response(verify_token(request.data["token"]), status=status.HTTP_200_OK)
 
     @action(methods=["POST"], detail=False,url_path="pass-reset",url_name="pass-reset", serializer_class=PasswordResetSerializer)
     def password_reset(self, request):
-        try:
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid()
-            username_or_email = serializer.validated_data["username_or_email"]
-            user = get_object_or_404(User, Q(email=username_or_email) | Q(username=username_or_email))
-            send_reset_email(user)
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Exception:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username_or_email = serializer.validated_data["username_or_email"]
+        user = get_object_or_404(User, Q(email=username_or_email) | Q(username=username_or_email))
+        send_reset_email(user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(methods=["POST"], detail=False, url_path="refresh", url_name="refresh", serializer_class=RefreshTokenSerializer)
     def refresh(self, request):
@@ -97,26 +90,18 @@ class UserViewSet(ModelViewSet):
     @action(methods=["POST"], detail=False, url_path="logout", url_name="logout", serializer_class=LogoutSerializer)
     def logout(self, request):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data["user"]
-            user.is_connected = False
-            user.save()
-            refresh_token = RefreshToken.objects.get(token=serializer.validated_data["token"])
-            refresh_token.delete()
-            return Response(status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @action(
-        methods=["POST"],
-        detail=False,
-        url_path="change-password",
-        url_name="change-password",
-        serializer_class=ChangePasswordSerializer,
-    )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        user.is_connected = False
+        user.save()
+        refresh_token = RefreshToken.objects.get(token=serializer.validated_data["token"])
+        refresh_token.delete()
+        return Response(status=status.HTTP_200_OK)
+    
+    @action(methods=["POST"], detail=False, url_path="change-password", url_name="change-password",serializer_class=ChangePasswordSerializer)
     def change_password(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         user: User = serializer.validated_data
         user.set_password(request.data.get("new_password"))
         user.save()
@@ -125,9 +110,7 @@ class UserViewSet(ModelViewSet):
     @action(methods=["POST"], detail=False, url_path="oauth", url_name="oauth", serializer_class=OAuthCodeSerializer)
     def oauth(self, request):
         serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.is_valid(raise_exception=True)
         code = serializer.validated_data["code"]
         access_token = get_access_token(code)
         if not access_token:
@@ -140,7 +123,4 @@ class UserViewSet(ModelViewSet):
         user = get_or_create_user(user_info)
         login_serializer = LoginSerializer(user)
 
-        return Response(
-            login_serializer.data,
-            status=status.HTTP_200_OK,
-        )
+        return Response(login_serializer.data, status=status.HTTP_200_OK)
