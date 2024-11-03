b398ec5723e1bf2b8997bdd13b233e2ecce4f0b9
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-11-03 11:12:35 +0100

    refactor: reaccomodate migrations

--- apps/user/migrations/0011_friendship_user_friends.py
+++ apps/user/migrations/0011_friendship_user_friends.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0007_user_two_factor_enabled'),
+        ('user', '0010_user_is_verified'),
     ]
 
     operations = [
