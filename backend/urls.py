09c1897e1d15e95b3b77280421f194d0f500c071
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-11-03 13:30:40 +0100

    feat: config files and add file endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("backend.urls_v1")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
