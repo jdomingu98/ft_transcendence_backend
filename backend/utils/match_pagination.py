5b4d590578ef661ec72055799a70050158c2be46
alextrujillo <alextrujillo72002@gmail.com>
2024-11-05 12:29:58 +0100

    refactor: rename file to match_pagination.py and streamline logic

--- backend/utils/match_pagination.py
+++ backend/utils/match_pagination.py
@@ -1,16 +1,8 @@
-from django.shortcuts import get_object_or_404
-from django.db.models import Q
 from rest_framework.pagination import PageNumberPagination
 from apps.user.v1.serializers import LocalMatchSerializer
-from apps.user.models import User
-from apps.game.models import LocalMatch
 
 class UserMatchPagination(PageNumberPagination):
     page_size = 5
-
-def fetch_user_matches(user_id):
-    user = get_object_or_404(User, pk=user_id)
-    return LocalMatch.objects.filter(Q(user_a=user) | Q(user_b=user))
 
 def paginate_matches(matches, request):
     paginator = UserMatchPagination()
@@ -23,7 +15,3 @@ def paginate_matches(matches, request):
         'numItems': paginator.page_size,
         'matches': serializer.data
     })
-
-def get_user_matches(user_id, request):
-    matches = fetch_user_matches(user_id)
-    return paginate_matches(matches, request)
